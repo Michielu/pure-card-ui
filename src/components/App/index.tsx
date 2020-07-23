b47a56c33072be984ea51c29cc2f03503f4fab2e
Michielu Menning <mich.menning@gmail.com>
2020-07-22 19:48:10 -0500

    Restructure and initial styling

---
+++ src/components/App/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Home } from '../index';
+
+function App() {
+  return (
+    <div className="app-body row">
+      <div className="app-content col-12 col-md-9">
+        <Home></Home>
+      </div>
+    </div>
+  );
+}
+
+export default App;
