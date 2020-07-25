4b8eaadcceb2ce7342bb5691cea286a73a342509
Michielu Menning <mich.menning@gmail.com>
2020-07-25 16:37:43 -0500

    Add routes

---
+++ src/utils/Router/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+    BrowserRouter as BRouter,
+    Switch,
+    Route
+} from "react-router-dom";
+
+import { About, Cards, Home } from '../../components';
+
+export default function Router() {
+    return (
+        <BRouter>
+            <div>
+                {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/cards">
+                        <Cards />
+                    </Route>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                </Switch>
+            </div>
+        </BRouter>
+    );
+}
