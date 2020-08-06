e30663a1edd1e3a24bfec257eac880a0db0dd3a1
Michielu Menning <mich.menning@gmail.com>
2020-08-05 20:36:50 -0500

    Remove idb file

--- src/utils/Storage/DBStorage/idb.js
+++
@@ -1,7 +0,0 @@
-import { openDB } from "idb";
-
-const DB_NAME = "db1";
-
-export const idb = {
-    db1: openDB(DB_NAME, 1)
-};
--- src/utils/Storage/IDB/idb.js
+++
@@ -1,7 +0,0 @@
-import { openDB } from "idb";
-
-import { DB_NAME } from './consts';
-
-export const idb = {
-    db1: openDB(DB_NAME, 1)
-};
