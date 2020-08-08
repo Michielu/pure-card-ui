fdcdc8d22bfcd9f58d74c223ab4f8041fe0074a7
Michielu Menning <mich.menning@gmail.com>
2020-08-07 19:15:13 -0500

    Add categories

---
+++ src/utils/data/categories.js
@@ -0,0 +1,23 @@
+const Staples = [
+    "gas",
+    "grocery",
+    "fast food",
+    "resturant",
+    "home improvement",
+    "travel"
+];
+
+const Specialties = [
+    "amazon",
+    "cell phone",
+    "home utilities",
+    "drug stores",
+    "airlines",
+    "ride share",
+    "dept stores",
+    "wholesale clubs"
+]
+
+export {
+    Staples, Specialties
+}
