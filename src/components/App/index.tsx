11f73bfc61009f2a048c26ce3deabd20145a21bb
Michielu Menning <mich.menning@gmail.com>
2020-08-11 20:59:16 -0500

    Ensure idb load before page load

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,25 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Home } from '../index';
 import { PCNav } from '../../partials';
 import Router from '../../utils/Router';
 import { setupStore } from '../../utils/IDB';
 
 function App() {
+  const [isDbSetup, setDbSetup] = useState(false);
 
   useEffect(() => {
-    setupStore();
+    async function setupIDB() {
+      await setupStore();
+      setDbSetup(true);
+    }
+
+    setupIDB();
   }, [])
 
   return (
     <div>
-      <PCNav />
-      <div className="app-body row">
-        <div className="app-content col-12 col-md-9">
-          <Router></Router>
-        </div>
-      </div>
+      {isDbSetup ?
+        <div>
+          <PCNav />
+          <div className="app-body row">
+            <div className="app-content col-12 col-md-9">
+              <Router ></Router>
+            </div>
+          </div>
+        </div> :
+        <div> Loading...</div >
+      }
     </div>
+
   );
 }
 
