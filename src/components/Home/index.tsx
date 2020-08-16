be7545815ab7295752cc5c851bfc929329152f76
Michielu Menning <mich.menning@gmail.com>
2020-08-16 08:44:55 -0500

    Get cards in homepage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-// import { Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 
-import { addToStore, getFromStore, getAllFromStore } from '../../utils/IDB';
+import { getAllFromCardStore, addToCardStore } from '../../utils/IDB';
+import { PCAddCard } from '../../partials';
 
 function Home() {
+    //TODO use custom hook
+    const [card, setCard] = useState([]);
+
+    useEffect(() => {
+        async function getCards() {
+            const c = await getAllFromCardStore();
+            setCard(c);
+
+        };
+        getCards();
+    }, []);
+
+    async function huh() {
+        const rv = await getAllFromCardStore();
+        console.log("RV", rv);
+    }
+
     return (
         <div>
-            Hom
-            {/* <button onClick={() => addToStore({ key: "key1", value: ["a", "1"] })}>Post to store</button>
-            <button onClick={() => console.log("Get store", getFromStore("key1"))}>Get from store</button> */}
+            {card.length > 0 ? <div>Contains card</div> : <PCAddCard></PCAddCard>}
+
+
+            <button onClick={() => addToCardStore({ key: "key1", value: ["a", "1"] })}>Post to store</button>
+            <button onClick={() => huh()}>Get from store</button>
 
         </div >
     );
