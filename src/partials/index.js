3b4c155862d3858971909732b58c602a3697c765
Michielu Menning <mich.menning@gmail.com>
2020-08-20 21:32:13 -0500

    Add accordians to index

--- src/partials/index.js
+++ src/partials/index.js
@@ -1,3 +1,4 @@
+import PCAccordian from './PCAccordion';
 import PCAddCard from './PCAddCard';
 import PCButton from './PCButton';
 import PCCards from './PCCards';
@@ -7,5 +8,5 @@ import PCNav from './PCNav';
 
 
 export {
-    PCAddCard, PCButton, PCCards, PCCardForm, PCNav
+    PCAccordian, PCAddCard, PCButton, PCCards, PCCardForm, PCNav
 }
