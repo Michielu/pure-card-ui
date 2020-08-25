d28521bce5f46d94013174df7a0ecae79717da0c
Michielu Menning <mich.menning@gmail.com>
2020-08-25 08:50:30 -0500

    Cards page

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getAllFromCardStore } from '../../utils/IDB';
+import { PCAddCard, PCCards } from '../../partials';
+import AddButton from '../../partials/PCAddCard/addButton';
 
 function Cards() {
-    return (
-        <div>
-            Cards
-        </div>
-    )
+    const [card, setCard] = useState([]);
+
+    useEffect(() => {
+        async function getCards() {
+            const c = await getAllFromCardStore();
+            setCard(c);
+        };
+        getCards();
+    }, []);
+
+    function generateCards() {
+        if (card.length > 0) {
+            return (
+                <div>
+                    <PCCards cardIds={card[0]}></PCCards>
+                    <button>edit buttons (remove or add card)</button>
+                    <AddButton></AddButton>
+                </div >
+            )
+        }
+        return <PCAddCard></PCAddCard>
+    }
+
+    return generateCards();
 }
 
 export default Cards;
