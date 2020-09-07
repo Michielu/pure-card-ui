d675ed6c60af587da29565552013cbab3f0204c0
Michielu Menning <mich.menning@gmail.com>
2020-09-07 18:29:56 -0500

    Add shop to partial index

--- src/partials/index.js
+++ src/partials/index.js
@@ -4,9 +4,10 @@ import PCButton from './PCButton';
 import PCCards from './PCCards';
 import PCCardForm from './PCCardForm';
 import PCNav from './PCNav';
+import PCShop from './PCShop';
 
 
 
 export {
-    PCAccordian, PCAddCard, PCButton, PCCards, PCCardForm, PCNav
+    PCAccordian, PCAddCard, PCButton, PCCards, PCCardForm, PCNav, PCShop
 }
