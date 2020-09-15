d3365dc5a9125e2f2430e6b05f30290b477e6bc9
Michielu Menning <mich.menning@gmail.com>
2020-09-14 19:41:03 -0500

    Clean up shop page

--- src/partials/PCShop/index.tsx
+++ src/partials/PCShop/index.tsx
@@ -2,19 +2,65 @@ import React, { useState, useEffect } from 'react';
 import { Form } from 'react-bootstrap';
 
 import { staples, specialties } from '../../utils/data/categories';
-import { getFromStaplesStore, getFromSpecialtiesStore, getFromCardStore } from '../../utils/IDB';
+import {
+  getFromStaplesStore,
+  // getFromSpecialtiesStore,
+  getFromCardStore,
+} from '../../utils/IDB';
 
-// interface ShopCard {
-//     name: string;
-//     value: number;
-// }
+interface ShopCard {
+    name: string;
+    value: number;
+}
 
 const categories = staples.concat(specialties);
 
+const getAllCardPercentage = async (item) => {
+  return getFromStaplesStore(item);
+};
+
+const getValuesFromCategory = async (cardNames: string[], category: string) => {
+  const stapleTitles = await getFromStaplesStore('titles');
+  //   const specialtiesTitles = await getFromSpecialtiesStore('titles');
+  //   const inStaple = stapleTitles.includes(category);
+
+  // const categoryValuePair: ShopCard[] = [];
+
+  // Check if category is in staple
+  //   if (inStaple) {
+  const index = stapleTitles.findIndex((cat: string) => cat === category);
+
+  console.log('cardNames:', cardNames);
+
+  console.log('In staples', index);
+  const returnableCardValue = await Promise.all(cardNames.map((caa: string) => {
+    return getAllCardPercentage(caa);
+  }));
+
+  console.log('returnableCard', returnableCardValue);
+
+  return cardNames.map((name) => ({ name, value: returnableCardValue[index] }));
+  //   }
+  //   const index = specialtiesTitles.findIndex((cat: string) => cat === category);
+
+  //   console.log('is specialty', index);
+  //   return cards.map(async (card: string) => {
+  //     const value = await getFromSpecialtiesStore(card);
+  //     return { name: card, value };
+  //   });
+
+  // Check if category is in specialty
+  // console.log("Returning pairs: ", categoryValuePair)
+  // return categoryValuePair;
+  // return [{
+  //     name: "hi",
+  //     value: 3
+  // }]
+};
+
 const PCShop = () => {
   const [categoryShop, setCategoryShop] = useState('');
-  const [cards, setCards] = useState([]);
-  // const [storedCategories, setStoredCategories] = useState(null);
+  const [cards, setCards] = useState([] as ShopCard[]);
 
   const generateOptions = () => {
     let value = 0;
@@ -28,29 +74,35 @@ const PCShop = () => {
   };
 
   useEffect(() => {
-    async function newShopCategory() {
+    async function generateCardValues() {
       console.log('Print: ', categories, categoryShop);
       const category = categories[parseInt(categoryShop)];
 
       const c = await getFromCardStore('cards');
-      const cardsWithValue = await getValuesFromCategory(cards, category);
-      console.log('new shop category: ', cardsWithValue);
+      console.log("Cards!: ", c);
+         await getValuesFromCategory(c, category);
+    //   console.log('new shop category: ', cardsWithValue);
+      setCards([{'name':'name1', 'value':2}]);
 
-      setCards(c);
+    //   setCards(c);
       // setStoredCategories(cardsWithValue)
     }
 
-    newShopCategory();
+    generateCardValues();
   }, [categoryShop]);
 
   const generateCardsInOrder = () => {
     if (categoryShop === '') return null;
     const category = categories[parseInt(categoryShop)];
 
+
     // Get all cards
     // Get category index in title
     // Get all card's values for category
     // Sort cards from greatest to smallest
+
+    console.log('Cards are: ', cards);
+
     return (
       <div>
         Display category:
@@ -58,40 +110,6 @@ const PCShop = () => {
         {category}
       </div>
     );
-  };
-
-  const getValuesFromCategory = async (cards: string[], category: string) => {
-    const stapleTitles = await getFromStaplesStore('titles');
-    const specialtiesTitles = await getFromSpecialtiesStore('titles');
-    const inStaple = stapleTitles.includes(category);
-
-    // const categoryValuePair: ShopCard[] = [];
-
-    // Check if category is in staple
-    if (inStaple) {
-      const index = stapleTitles.findIndex((cat: string) => cat === category);
-
-      console.log('In staples', index);
-      return cards.map(async (card: string) => {
-        const value = await getFromStaplesStore(card);
-        return { name: card, value };
-      });
-    }
-    const index = specialtiesTitles.findIndex((cat: string) => cat === category);
-
-    console.log('is specialty', index);
-    return cards.map(async (card: string) => {
-      const value = await getFromSpecialtiesStore(card);
-      return { name: card, value };
-    });
-
-    // Check if category is in specialty
-    // console.log("Returning pairs: ", categoryValuePair)
-    // return categoryValuePair;
-    // return [{
-    //     name: "hi",
-    //     value: 3
-    // }]
   };
 
   return (
