03ad3de77d8db6cdeff0ab82e18d62842bc33fa9
Michielu Menning <mich.menning@gmail.com>
2020-09-17 20:04:22 -0500

    Reformat categories to match linter

--- src/utils/data/categories.js
+++ src/utils/data/categories.js
@@ -1,19 +1,19 @@
 const required = [
-    "Card Name", "Default Percent"
-]
+  'Card Name', 'Default Percent',
+];
 
 const staples = [
-    "Gas", "Grocery", "Fast Food",
-    "Resturant", "Home improvement",
-    "Travel"
-]
+  'Gas', 'Grocery', 'Fast Food',
+  'Resturant', 'Home improvement',
+  'Travel',
+];
 
 const specialties = [
-    "Amazon", "Cell phone", "Drug store",
-    "Home Utils", "Airlines", "Ride shares",
-    "Dept stores", "Wholesale"
-]
+  'Amazon', 'Cell phone', 'Drug store',
+  'Home Utils', 'Airlines', 'Ride shares',
+  'Dept stores', 'Wholesale',
+];
 
 export {
-    required, staples, specialties
-}
+  required, staples, specialties,
+};
